33e8989989a696542f7984b57e38c60a642cf567
secretquantum <104043642+secretquantum@users.noreply.github.com>
2022-09-26 22:24:48 -0400

    Pointers

---
+++ include/functions.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+
+void mathTest(int a, int b) {
+    int math = a * b;
+
+    std::cout << math;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include "functions.h"
+#include <cstring>
+
 
 using namespace std;
+#define LOG(x) std::cout << x << std::endl;
 
 int main() {
 
-    cout << "Hello world";
+    cout << "Hello world" << '\n';
+    cout << "Function MathTest: ";
+    mathTest(3 , 8);
+    cout << '\n';
+
+    
+    int var = 8; // allocating memory on the stack
+    int* ptr = &var;
+    *ptr = 10;
+    LOG(var);
+    cout << ptr << '\n';
+
+    char* buffer = new char[8];
+    memset(buffer, 1, 8);
+    delete[] buffer;
 
     return 0;
 }
